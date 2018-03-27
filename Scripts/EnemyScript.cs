f5334ce787058245b9e7ffe13cbb4e4f316ede9a
wanglei98 <31127216+wanglei98@users.noreply.github.com>
2018-03-27 22:36:47 +0800

    Add files via upload

---
+++ Scripts/EnemyScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	private WeaponScript[] weapons;
+
+	void Awake ()
+	{
+		weapons = GetComponentsInChildren<WeaponScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		foreach (WeaponScript weapon in weapons) {
+			
+			if (weapon != null && weapon.CanAttack) {
+				weapon.Attack (true);
+			}
+		}
+	}
+}
---
+++ Scripts/PlayerScripts.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScripts : MonoBehaviour {
+
+	// Use this for initialization
+	public Vector2 speed = new Vector2(50,50);
+
+	private Vector2 movement;
+
+	void Update ()
+	{
+		bool shoot = Input.GetButtonDown ("Fire1");
+		shoot |= Input.GetButtonDown ("Fire2");
+
+		if (shoot) {
+			WeaponScript weapon = GetComponent<WeaponScript> ();
+			if (weapon != null) {
+
+				weapon.Attack (false);
+			}
+		}
+
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+		movement = new Vector2 (
+			speed.x * inputX,
+			speed.y * inputY);
+		
+	}
+
+	void FixedUpdate()
+	{
+		GetComponent<Rigidbody2D> ().velocity = movement;
+	}
+
+}
